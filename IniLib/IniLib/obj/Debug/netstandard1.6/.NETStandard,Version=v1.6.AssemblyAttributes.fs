3cd92c94ce668a5716d907386e1427a8a818c1b3
marriola <azathoth500@gmail.com>
2023-01-01 03:53:16 -0500

    Removed obj directory

--- IniLib/IniLib/obj/Debug/netstandard1.6/.NETStandard,Version=v1.6.AssemblyAttributes.fs
+++
@@ -1,3 +0,0 @@
-namespace Microsoft.BuildSettings
-                [<System.Runtime.Versioning.TargetFrameworkAttribute(".NETStandard,Version=v1.6", FrameworkDisplayName=".NET Standard 1.6")>]
-                do ()
